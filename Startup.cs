1fb408cf04016cbbae68e17a3dffa69fa658cf08
antoha22 <42731581+antoha22@users.noreply.github.com>
2020-04-08 22:37:29 +0300

    Работа с конфигурацией

--- Startup.cs
+++ Startup.cs
@@ -7,14 +7,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace WebStore_2020
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,13 +33,16 @@ namespace WebStore_2020
 
             app.UseRouting();
 
+            string helloMsg = configuration["CustomHelloWorld"];
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(helloMsg);
                 });
             });
+
         }
     }
 }
